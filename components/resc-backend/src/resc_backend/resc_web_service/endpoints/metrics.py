60fbb7d0e6cc334814450ba27af0daa3371f193e
Peter-v-d-Spek <peter.van.der.spek@nl.abnamro.com>
2023-06-14 11:01:50 +0200

    [#2376085] Metrics add audit count by auditor

--- components/resc-backend/src/resc_backend/resc_web_service/endpoints/metrics.py
+++ components/resc-backend/src/resc_backend/resc_web_service/endpoints/metrics.py
@@ -11,14 +11,17 @@ from resc_backend.constants import (
     ERROR_MESSAGE_500,
     ERROR_MESSAGE_503,
     METRICS_TAG,
+    RWS_ROUTE_AUDIT_COUNT_BY_AUDITOR_OVER_TIME,
     RWS_ROUTE_AUDITED_COUNT_OVER_TIME,
     RWS_ROUTE_COUNT_PER_VCS_PROVIDER_BY_WEEK,
     RWS_ROUTE_METRICS,
     RWS_ROUTE_UN_TRIAGED_COUNT_OVER_TIME
 )
 from resc_backend.db.connection import Session
+from resc_backend.resc_web_service.crud import audit as audit_crud
 from resc_backend.resc_web_service.crud import finding as finding_crud
 from resc_backend.resc_web_service.dependencies import get_db_connection
+from resc_backend.resc_web_service.schema.audit_count_over_time import AuditCountOverTime
 from resc_backend.resc_web_service.schema.finding_count_over_time import FindingCountOverTime
 from resc_backend.resc_web_service.schema.finding_status import FindingStatus
 from resc_backend.resc_web_service.schema.vcs_provider import VCSProviders
@@ -141,4 +144,49 @@ def convert_rows_to_finding_count_over_time(count_over_time: dict, weeks: int) -
             setattr(week_data.vcs_provider_finding_count, vcs_provider_type, week_groups[week][vcs_provider_type])
 
         output.append(week_data)
+    return output
+
+
+@router.get(f"{RWS_ROUTE_AUDIT_COUNT_BY_AUDITOR_OVER_TIME}",
+            response_model=list[AuditCountOverTime],
+            summary="Get count of Audits by Auditor over time for given weeks",
+            status_code=status.HTTP_200_OK,
+            responses={
+                200: {"description": "Retrieve count of Audits by Auditor over time for given weeks"},
+                500: {"description": ERROR_MESSAGE_500},
+                503: {"description": ERROR_MESSAGE_503}
+            })
+def get_audit_count_by_auditor_over_time(db_connection: Session = Depends(get_db_connection),
+                                         weeks: Optional[int] = Query(default=13, ge=1)) \
+        -> list[AuditCountOverTime]:
+    """
+        Retrieve count of Audits by Auditor over time for given weeks
+    - **db_connection**: Session of the database connection
+    - **weeks**: Nr of weeks for which to retrieve the audit counts
+    - **return**: [AuditCountOverTime]
+        The output will contain a list of AuditCountOverTime type objects
+    """
+    audit_counts = audit_crud.get_audit_count_by_auditor_over_time(db_connection=db_connection, weeks=weeks)
+
+    # get the unique auditors from the data
+    auditors_default = {}
+    for audit in audit_counts:
+        auditors_default[audit['auditor']] = 0
+
+    # default to 0 per auditor for all weeks in range
+    weekly_audit_counts = {}
+    for week in range(0, weeks):
+        nth_week = datetime.utcnow() - timedelta(weeks=week)
+        week = f"{nth_week.isocalendar().year} W{nth_week.isocalendar().week:02d}"
+        weekly_audit_counts[week] = AuditCountOverTime(time_period=week, audit_by_auditor_count=dict(auditors_default))
+    weekly_audit_counts = dict(sorted(weekly_audit_counts.items()))
+
+    # set the counts based on the data from the database
+    for audit in audit_counts:
+        audit_week = f"{audit['year']} W{audit['week']:02d}"
+        weekly_audit_counts.get(audit_week).audit_by_auditor_count[audit['auditor']] = audit['audit_count']
+        weekly_audit_counts.get(audit_week).total += audit['audit_count']
+
+    sorted_weekly_audit_counts = dict(sorted(weekly_audit_counts.items()))
+    output = list(sorted_weekly_audit_counts.values())
     return output
