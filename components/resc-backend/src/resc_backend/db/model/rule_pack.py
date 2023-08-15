07e87b9e8c23f424609113f8afef8f28c47d8a2b
Peter-v-d-Spek <peter.van.der.spek@nl.abnamro.com>
2023-08-15 13:20:43 +0200

    [#2588985] Add created datetime to rulepack

--- components/resc-backend/src/resc_backend/db/model/rule_pack.py
+++ components/resc-backend/src/resc_backend/db/model/rule_pack.py
@@ -1,5 +1,8 @@
+# Standard Library
+from datetime import datetime
+
 # Third Party
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 
 # First Party
 from resc_backend.db.model import Base
@@ -11,17 +14,21 @@ class DBrulePack(Base):
     version = Column("version", String(100), primary_key=True)
     global_allow_list = Column(Integer, ForeignKey(DBruleAllowList.id_), nullable=True)
     active = Column(Boolean, nullable=False, default=False)
+    created = Column(DateTime, nullable=False, default=datetime.utcnow)
 
-    def __init__(self, version: str, global_allow_list: int = None, active: bool = False):
+    def __init__(self, version: str, global_allow_list: int = None, active: bool = False,
+                 created: datetime = datetime.utcnow()):
         self.version = version
         self.global_allow_list = global_allow_list
         self.active = active
+        self.created = created
 
     @staticmethod
-    def create_from_metadata(version: str, global_allow_list: int, active: bool):
+    def create_from_metadata(version: str, global_allow_list: int, active: bool, created):
         db_rule_pack = DBrulePack(
             version=version,
             global_allow_list=global_allow_list,
             active=active,
+            created=created
         )
         return db_rule_pack
