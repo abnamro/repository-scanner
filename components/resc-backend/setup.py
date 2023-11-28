7117ae48c055acb8576671fca5498fdb9cde23e9
mohammed Usman <mohammed.usman@nl.abnamro.com>
2023-11-28 20:39:19 +0530

    Removed requirements.txt and updated setup.cfg with installation libs

--- components/resc-backend/setup.py
+++ components/resc-backend/setup.py
@@ -2,11 +2,4 @@
 from setuptools import setup
 
 
-def get_required_packages():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
-
-setup(
- install_requires=get_required_packages()
-)
+setup()
