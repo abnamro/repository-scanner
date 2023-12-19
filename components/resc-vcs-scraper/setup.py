d9b72fee665d34c1bf16726c843397c0d2346d56
Usman2ABN <113449186+Usman2ABN@users.noreply.github.com>
2023-12-19 13:41:18 +0100

    Using build to build the sdist and wheel files (#200)
    * Using build to build the sdist and wheel files
    
    * Removed requirements.txt and updated setup.cfg with installation libs
    
    * Updated setup.cfg with installation libs

--- components/resc-vcs-scraper/setup.py
+++ components/resc-vcs-scraper/setup.py
@@ -1,15 +1,5 @@
-# Standard Library
-from os import getenv
-
 # Third Party
 from setuptools import setup
 
 
-def get_required_packages():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
-
-setup(
-    install_requires=get_required_packages()
-)
+setup()
