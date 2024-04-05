873e35197263f619df77ac205e360555944b6ef7
balanza-martinez <161321603+balanza-martinez@users.noreply.github.com>
2024-04-05 10:58:47 +0200

    Reverted back to requirement.txt (#219)
    * Reverted back to requirement.txt
    
    * Updated helm-wizard github workflow to reflect requirement.txt changes

--- deployment/resc-helm-wizard/setup.py
+++ deployment/resc-helm-wizard/setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
 
+def get_required_packages():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
 
-setup()
+setup(
+ install_requires=get_required_packages()
+)
